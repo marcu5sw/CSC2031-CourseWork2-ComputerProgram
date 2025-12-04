9afde03b8865d9f10be101d46521ce3fd424a252
marcu5sw <c3043563@newcastle.ac.uk>
2025-12-04 15:35:06 +0000

    Changing secret key. Now stored in .env file. Using dotenv library to load key from file

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 from cryptography.fernet import Fernet
 import os
+from dotenv import load_dotenv
 
 
 
 #Getting secret key from OS level (environmental variable)
-key = os.environ.get('SECRET_KEY')
+'''key = os.environ.get('SECRET_KEY')
 
-print (key)
+#print (key)
 
 #If there isn't an environmental variable called SECRET_KEY, Fernet to create one
 if key is None:
@@ -17,21 +18,26 @@ else:
     key = key.encode()
 
 
-print (key)
+#print (key)
 
 
-fernet = Fernet(key) #Initializing to be used elsewhere
+fernet = Fernet(key) #Initializing to be used elsewhere'''
+
+
+load_dotenv()
+SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
+print(SECRET_KEY)
 
 
 
 class Config:
     DEBUG = True
-    SECRET_KEY = key #Has to be string so set to key
+    SECRET_KEY = SECRET_KEY #Has to be string so set to key
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
 
 
-print(Config.SECRET_KEY)
+#print(Config.SECRET_KEY)
 
 
--- run.py
+++ run.py
@@ -1,6 +1,19 @@
 from app import create_app
+from cryptography.fernet import Fernet
+import os
+#from config import fernet
+
+
+#key = Fernet.generate_key()
+
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+    #print(key)
+    #print(fernet)
+
+
+
+
